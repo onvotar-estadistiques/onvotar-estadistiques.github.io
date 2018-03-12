c2f84e341083c83c0d6b16fe01a927cb2fdf3d07
Angel Fernandez <angelfernandezibanez@gmail.com>
2018-03-12 23:09:00 +0100

    Add age chart

--- js/base.js
+++ js/base.js
@@ -111,3 +111,120 @@ dateSlider.noUiSlider.on('update', function( values, handle ) {
     document.getElementById('timeslider-label').innerHTML = formatDate(new Date(+values[handle]));
     loadData();
 });
+
+var ctx = document.getElementById("ageChart").getContext("2d");
+var ageChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        datasets: [
+            {
+                fill: 'origin',
+                label: '# de consultes',
+                data: [
+                    {x: 18, y: 40},
+                    {x: 19, y: 94},
+                    {x: 20, y: 73},
+                    {x: 21, y: 70},
+                    {x: 22, y: 57},
+                    {x: 23, y: 65},
+                    {x: 24, y: 96},
+                    {x: 25, y: 77},
+                    {x: 26, y: 65},
+                    {x: 27, y: 64},
+                    {x: 28, y: 59},
+                    {x: 29, y: 55},
+                    {x: 30, y: 61},
+                    {x: 31, y: 71},
+                    {x: 32, y: 65},
+                    {x: 33, y: 51},
+                    {x: 34, y: 68},
+                    {x: 35, y: 56},
+                    {x: 36, y: 58},
+                    {x: 37, y: 64},
+                    {x: 38, y: 64},
+                    {x: 39, y: 65},
+                    {x: 40, y: 73},
+                    {x: 41, y: 78},
+                    {x: 42, y: 70},
+                    {x: 43, y: 73},
+                    {x: 44, y: 72},
+                    {x: 45, y: 51},
+                    {x: 46, y: 63},
+                    {x: 47, y: 49},
+                    {x: 48, y: 54},
+                    {x: 49, y: 56},
+                    {x: 50, y: 65},
+                    {x: 51, y: 54},
+                    {x: 52, y: 62},
+                    {x: 53, y: 58},
+                    {x: 54, y: 54},
+                    {x: 55, y: 39},
+                    {x: 56, y: 57},
+                    {x: 57, y: 48},
+                    {x: 58, y: 46},
+                    {x: 59, y: 40},
+                    {x: 60, y: 30},
+                    {x: 61, y: 30},
+                    {x: 62, y: 18},
+                    {x: 63, y: 23},
+                    {x: 64, y: 25},
+                    {x: 65, y: 30},
+                    {x: 66, y: 17},
+                    {x: 67, y: 16},
+                    {x: 68, y: 19},
+                    {x: 69, y: 15},
+                    {x: 70, y: 13},
+                    {x: 71, y: 9},
+                    {x: 72, y: 10},
+                    {x: 73, y: 4},
+                    {x: 74, y: 4},
+                    {x: 75, y: 4},
+                    {x: 76, y: 10},
+                    {x: 77, y: 4},
+                    {x: 78, y: 3},
+                    {x: 79, y: 1},
+                    {x: 80, y: 5},
+                    {x: 81, y: 8},
+                    {x: 82, y: 5},
+                    {x: 83, y: 3},
+                    {x: 84, y: 7},
+                    {x: 85, y: 5},
+                    {x: 86, y: 2},
+                    {x: 87, y: 4},
+                    {x: 88, y: 1},
+                    {x: 89, y: 1},
+                    {x: 90, y: 0},
+                    {x: 91, y: 2},
+                    {x: 92, y: 3},
+                    {x: 93, y: 0},
+                    {x: 94, y: 0},
+                    {x: 95, y: 1},
+                    {x: 96, y: 0},
+                    {x: 97, y: 1},
+                ]
+            }
+        ]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }],
+            xAxes: [{
+                type: 'linear',
+                position: 'bottom',
+                ticks: {
+                    min: 18,
+                    max: 97,
+                    stepSize: 5,
+                },
+                scaleLabel: {
+                    display: true,
+                    labelString: 'Edat'
+                }
+            }]
+        }
+    }
+});
